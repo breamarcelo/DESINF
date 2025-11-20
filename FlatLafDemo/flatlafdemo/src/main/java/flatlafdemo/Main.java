ee0867c5ca62d2ea4d89e9bfd0cfac9737b3f48e
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-20 20:44:46 +0100

    funcionando UIManager.put()

--- FlatLafDemo/flatlafdemo/src/main/java/flatlafdemo/Main.java
+++ FlatLafDemo/flatlafdemo/src/main/java/flatlafdemo/Main.java
@@ -2,15 +2,15 @@ package flatlafdemo;
 
 import javax.swing.UIManager;
 
-import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 
 public class Main {
     public static void main(String[] args) {
-        FlatDarkLaf.setup();
-        MiJFrame frame = new MiJFrame();
-
+        FlatLightLaf.setup();
         UIManager.put("Component.arc", 12);
         UIManager.put("Button.arc", 14);
         UIManager.put("TextComponent.arc", 10);
+        
+        MiJFrame frame = new MiJFrame();
     }
 }
--- FlatLafDemo/flatlafdemo/src/main/java/flatlafdemo/MiJPanel.java
+++ FlatLafDemo/flatlafdemo/src/main/java/flatlafdemo/MiJPanel.java
@@ -5,10 +5,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class MiJPanel extends JPanel{
     public MiJPanel(){
         add(new JLabel("Hola"));
         add(new JTextField());
         add(new JButton("Hola"));
+
+        
+
+        
     }
 }
