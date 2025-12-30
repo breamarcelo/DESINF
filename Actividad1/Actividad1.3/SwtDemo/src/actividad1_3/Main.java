7f207b8265248a8dde537f3c978e962697641476
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-12-30 03:52:08 +0100

    iniciada Actividad1.3 SWT

---
+++ Actividad1/Actividad1.3/SwtDemo/src/actividad1_3/Main.java
@@ -0,0 +1,21 @@
+package actividad1_3;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Hello World!");
+		shell.open();
+		
+		while(!shell.isDisposed()) {
+			if(!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
+	}
+
+}
